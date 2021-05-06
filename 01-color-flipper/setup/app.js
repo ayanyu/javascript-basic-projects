73a88eb9898066b22db0d4f76b14dac06e984181
Ayan Haibeh <ayanyusufh@gmail.com>
2021-05-06 11:55:35 -0400

    added color flipper

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,19 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+
+btn.addEventListener('click', () =>{
+    //get random number between zero and three
+    const randomNum = getRandomNum();
+    console.log(randomNum);
+
+    document.body.style.backgroundColor = colors[randomNum];
+    color.textContent = colors[randomNum]
+
+
+});
+
+function getRandomNum(){
+    return Math.floor(Math.random() * colors.length);
+}
